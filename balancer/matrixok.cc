e5ca5d94e5ea615e48b2f9f1b52e8802ab175da7
YoggYG <ajochembaas@gmail.com>
2018-02-21 17:14:56 +0100

    finished matrixok

--- balancer/matrixok.cc
+++ balancer/matrixok.cc
@@ -7,29 +7,29 @@ bool Balancer::matrixOK(size_t pos)
 
 	char val = d_matrix[pos];
 	char left = d_matrix[pos - 1];
-	char up = NB;
-	char upUp = NB;
-	char upLeft = NB;
-	char upRight = NB;
-	char leftLeft = NB;
+	char up = BN;
+	char upUp = BN;
+	char upLeft = BN;
+	char upRight = BN;
+	char leftLeft = BN;
 
 	if (pos > 1)
 		leftLeft = d_matrix[pos - 2];
 
+	if (pos > d_cols)
+		upLeft = d_matrix[pos - d_cols - 1];
+	
 	if (pos >= d_cols)
 	{
 		up = d_matrix[pos - d_cols];
 		upRight = d_matrix[pos - d_cols + 1];
 	}
 	
-	if (pos > d_cols)
-		upLeft = d_matrix[pos - d_cols - 1];
-
 	if (pos >= 2 * d_cols)
 		upUp = d_matrix[pos - 2 * d_cols];
 
 	if (val == EMPTY)
-		return not (hasEastOutput(left) or hasSouthOutput(up) or requiresWestInput(left) or requiresNorthInput(up));
+		return not hasEastOutput(left) and not hasSouthOutput(up) and not requiresWestInput(left) and not requiresNorthInput(up);
 
 	if (val == BN)
 		return hasNorthInput(up) and not requiresWestInput(left) and not hasEastOutput(upLeft) and not hasSouthOutput(upUp) and not hasWestOutput(upRight);
@@ -56,6 +56,16 @@ bool Balancer::matrixOK(size_t pos)
 		return not hasEastOutput(left) and not requiresWestInput(left) and not hasSouthOutput(up) and not requiresNorthInput(up) and not hasSouthOutput(upRight);
 
 	if (val == UBIW)
-		return not hasSouthOutput(up) and not requiresNorthInput(up) and not hasEastOutput(left) and not requiresWestInput(left) // maybe same as UBIN
+		return not hasSouthOutput(up) and not requiresNorthInput(up) and not hasEastOutput(left) and not requiresWestInput(left); // maybe same as UBIN
+
+	if (val == UBOW)
+		return hasWestInput(left) and not hasSouthOutput(up) and not requiresNorthInput(up) and not hasEastOutput(leftLeft) and not hasSouthOutput(upLeft);
+
+	if (val == UBIS)
+		return hasSouthOutput(up) and not hasEastOutput(left) and not requiresWestInput(left);
+
+	if (val == UBOS)
+		return not hasEastOutput(left) and not hasSouthOutput(up) and not requiresWestInput(left) and not requiresNorthInput(up);
+
 
 }
