6189f58e42285a650bde3c02d6d465472afabfc3
YoggYG <ajochembaas@gmail.com>
2018-04-19 16:20:28 +0200

    added horizontal underground belt helper functions for use in matrixOK

--- balancer/isedgecase.cc
+++ balancer/isedgecase.cc
@@ -10,21 +10,25 @@ bool Balancer::isEdgeCase(size_t idx)
 		if (d_matrix[idx] == BS or d_matrix[idx] == UBON or d_matrix[idx] == UBIS)
 			return true;
 
-	if (idx % d_cols == 0)
-		if (d_matrix[idx] == BW or d_matrix[idx] == UBIW or d_matrix[idx] == UBOW or d_matrix[idx] == UBIE or d_matrix[idx] == UBOE)
+	if (idx % d_cols < 3)
+	{
+		if (d_matrix[idx] == UBIW or d_matrix[idx] == UBOE)
 			return true;
+		
+		if (idx % d_cols == 0)
+			if (d_matrix[idx] == BW or d_matrix[idx] == UBOW or d_matrix[idx] == UBIE)
+				return true;
+	}
 
-	if (idx % d_cols == d_cols - 1)
-		if (d_matrix[idx] == BE or d_matrix[idx] == UBIW or d_matrix[idx] == UBOW or d_matrix[idx] == UBIE or d_matrix[idx] == UBOE)
-			return true;
-
-	if (idx % d_cols == d_cols - 2)
+	if (idx % d_cols > d_cols - 4)
+	{
 		if (d_matrix[idx] == UBIE or d_matrix[idx] == UBOW)
 			return true;
 
-	if (idx % d_cols == 1)
-		if (d_matrix[idx] == UBIW or d_matrix[idx] == UBOE)
-			return true;
+		if (idx % d_cols == d_cols - 1)
+			if (d_matrix[idx] == BE or d_matrix[idx] == UBIW or d_matrix[idx] == UBOE)
+				return true;
+	}
 
 	return false;
 }
