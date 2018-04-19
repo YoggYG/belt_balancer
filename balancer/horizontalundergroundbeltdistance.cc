255475b4d956b90d1ce0c2c39eb962a0782b39bf
YoggYG <ajochembaas@gmail.com>
2018-04-19 16:38:21 +0200

    fix missing !=

--- balancer/horizontalundergroundbeltdistance.cc
+++ balancer/horizontalundergroundbeltdistance.cc
@@ -2,8 +2,9 @@
 
 size_t Balancer::horizontalUndergroundBeltDistance(size_t pos)
 {
-	for (size_t distance = 2; distance < d_underground_length or (pos - distance) % d_cols == 0; ++distance)
+	for (size_t distance = 2; distance < d_underground_length or (pos - distance) % d_cols != 0; ++distance)
 		if (d_matrix[pos - distance] == UBIE or d_matrix[pos - distance] == UBOW)
 			return distance;
+	
 	return 0;
 }
