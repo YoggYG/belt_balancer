93285ee3c6fd5b1fba9903641333eedec986fb4a
YoggYG <ajochembaas@gmail.com>
2019-03-21 13:06:44 +0100

    trash removal

--- producer/operatorfun.cc
+++ producer/operatorfun.cc
@@ -11,24 +11,5 @@ void Producer::operator()(size_t n, size_t power, size_t rows, size_t cols, size
     size_t startIndex = cols + d_startNumber;
     size_t splittersToPlace = power * n / 2;
 
-	// vector<char> test
-	// {
-	// 	0,0,0,0,
-	// 	0,13,14,0,
-	// 	0,0,0,0,
-	// 	0,0,0,0,
-	// 	0,13,14,0,
-	// 	0,0,0,0,
-	// 	0,0,0,0,
-	// 	13,14,13,14,
-	// 	0,0,0,0
-	// };
-
-	// d_sdh.addTask(SharedDataHandler::Spec{Balancer(test, rows, cols, n, power), false});
-
     placeSplitter(matrix, startIndex, rows, cols, n, power, splittersToPlace, d_numIncrements, undergroundLength);
-    
-//     while (getNextMatrix(matrix, n, rows, cols))
-//     	if (possiblyValid(matrix, n, power))
-//         	d_sdh.addTask(SharedDataHandler::Spec{Balancer(matrix, rows, cols, n, power), false});
 }
